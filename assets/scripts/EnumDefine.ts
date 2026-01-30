276e1db47464d00e4fbc91016c19f109e8e61f90
v012345 <v012345@163.com>
2026-01-30 09:49:26 +0800

    状态机原型

---
+++ assets/scripts/EnumDefine.ts
@@ -0,0 +1,88 @@
+/**
+ * 角色状态
+ */
+export enum StateDefine {
+    Idle = "idle",
+    Attack = "attack",
+    MoveAttack = "moveAttack",
+    Move = "move",
+    Patrol = "patrol",
+    Die = "die",
+    /** 闪避 */
+    Dodge = "dodge",
+    /** 技能攻击 */
+    SkillAttack = "skillAttack",
+}
+/** 角色动画 */
+export enum AnimDefine {
+    /** 待机 */
+    Idle = "idle",
+    /** 移动 */
+    Move = "move",
+    /** 攻击 */
+    Attack = "attack",
+    /** 移动攻击 */
+    MoveAttack = "moveAttack",
+    /** 击退 */
+    Knockback = "knockback",
+    /** 闪避 */
+    SkillDodge = "skillDodge",
+    /** 技能攻击 */
+    SkillAttack = "skillAttack",
+    /** 死亡 */
+    Die = "die",
+    /** boss普通攻击 */
+    BossNormalAttack = "bossNormalAttack",
+    /** boss1技能攻击 */
+    BossSkill1Attack = "bossSkill1Attack",
+    /** boss2技能攻击 */
+    BossSkill2Attack = "bossSkill2Attack",
+    /** boss3技能攻击 */
+    BossSkill3Attack = "bossSkill3Attack",
+    /** 士兵待机 */
+    SoldierIdle = "soldierIdle",
+    /** 士兵移动 */
+    SoldierMove = "soldierMove",
+    /** 士兵攻击 */
+    SoldierAttack = "soldierAttack",
+    /** 士兵死亡 */
+    SoldierDie = "soldierDie",
+    /** 骑待机 */
+    RideIdle = "rideIdle",
+    /** 骑攻击 */
+    RideAttack = "rideAttack",
+    /** 骑死亡 */
+    RideDie = "rideDie",
+    /** 空中待机 */
+    AirIdle = "airIdle",
+    /** 空中移动 */
+    AirMove = "airMove",
+    /** 工作 */
+    Work = "work",
+    /** 出场 */
+    Appear = "appear",
+    /** 站立待机 */
+    StandIdle = "standIdle",
+}
+
+/** 角色阵营 */
+export enum GroupDefine {
+    /** 敌人 */
+    T = 'T',
+    /** 自己 */
+    B = 'B'
+}
+
+/** 音效类型 */
+export enum SoundType {
+
+    /** 收拿物品 */
+    收拿物品 = 'item',
+    /** 枪攻击 */
+    枪攻击 = 'qiangAtk',
+    /** 金币音效 */
+    金币 = 'coin',
+    /** 箭攻击 */
+    箭攻击 = 'jianAtk',
+}
+
--- assets/scripts/MainActor.ts
+++ assets/scripts/MainActor.ts
@@ -1,6 +1,7 @@
 import { _decorator, Component, instantiate, Node, Prefab, Vec3 } from 'cc';
 import { Bag } from './tools/Bag';
 import { GameGlobal } from './GameGlobal';
+import { StateDefine } from './EnumDefine';
 const { ccclass, property } = _decorator;
 
 @ccclass('MainActor')
@@ -12,6 +13,7 @@ export class MainActor extends Component {
     coinPrefab: Prefab;
     moveDir: Vec3 = new Vec3(0, 0, 0);
     isMoving: boolean = false;
+    currState: StateDefine = StateDefine.Idle;
     start() {
         GameGlobal.mainActor = this;
         this.getCoinPile();
@@ -21,12 +23,37 @@ export class MainActor extends Component {
         this.updateStateMachine(deltaTime);
     }
     updateStateMachine(deltaTime: number) {
-        // 状态机更新逻辑
-    };
-    /**
- * 设置角色移动
- * @param dir 移动方向
- */
+        if (this.isMoving) {
+            this.changeActorState(StateDefine.Move);
+        } else {
+            this.changeActorState(StateDefine.Idle);
+        }
+        switch (this.currState) {
+            case StateDefine.Idle:
+                this.onIdleState(deltaTime);
+                break;
+            case StateDefine.Move:
+                this.onMoveState(deltaTime);
+                break;
+        }
+    }
+    private onIdleState(deltaTime: number) {
+
+    }
+    private onMoveState(deltaTime: number) {
+
+        this.performMovement(deltaTime);
+
+    }
+
+    private performMovement(deltaTime: number) {
+
+        // Vec3.multiplyScalar(this.dir, this.moveDir, this.speed * deltaTime);
+        // this.characterController.move(this.dir);
+
+        // GameGlobal.cameraControl.cameraFollow();
+    }
+
     move(dir: Vec3) {
 
         this.moveDir = new Vec3(dir.x, dir.z, -dir.y);
@@ -38,8 +65,6 @@ export class MainActor extends Component {
         this.isMoving = false;
     }
     private getCoinPile() {
-        // 获取金币堆
-
         let coin = instantiate(this.coinPrefab);
         this.bagCoin.add(coin.getComponent(Component), 5, (isSuccess) => {
             if (isSuccess) {
@@ -49,6 +74,25 @@ export class MainActor extends Component {
         this.scheduleOnce(() => {
             this.getCoinPile();
         }, 2);
+    }
+    private changeActorState(newState: StateDefine) {
+        if (this.currState === newState) {
+            return;
+        }
+        this.exitState(this.currState);
+        const oldState = this.currState;
+        this.currState = newState;
+        this.enterState(newState, oldState);
+    }
+
+
+    private enterState(state: StateDefine, oldState: StateDefine | string) {
+
+    }
+
+
+    private exitState(state: StateDefine | string) {
+
     }
 }
 
